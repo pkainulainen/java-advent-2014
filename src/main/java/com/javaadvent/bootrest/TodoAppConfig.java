4598ff4bb816bbc0e2da8d4a4a748d56f0acf6d9
Petri Kainulainen <petri.kainulainen@gmail.com>
2014-12-02 18:11:52 +0200

    Add a basic Spring Boot application

---
+++ src/main/java/com/javaadvent/bootrest/TodoAppConfig.java
@@ -0,0 +1,28 @@
+package com.javaadvent.bootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This configuration class has three responsibilities:
+ * <ol>
+ *     <li>It enables the auto configuration of the Spring application context.</li>
+ *     <li>
+ *         It ensures that Spring looks for other components (controllers, services, and repositories) from the
+ *         <code>com.javaadvent.bootrest.todo</code> package.
+ *     </li>
+ *     <li>It launches our application in the main() method.</li>
+ * </ol>
+ * @author Petri Kainulainen
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.javaadvent.bootrest.todo")
+public class TodoAppConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppConfig.class, args);
+    }
+}
---
+++ src/main/java/com/javaadvent/bootrest/todo/TodoController.java
@@ -0,0 +1,13 @@
+package com.javaadvent.bootrest.todo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Petri Kainulainen
+ */
+@RestController
+@RequestMapping("/api/todo")
+public class TodoController {
+
+}
